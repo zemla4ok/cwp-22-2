3863350739150435cbc18decb48e3d193d889476
zemla4ok <dima-vital@live.com>
2018-02-19 20:51:34 +0300

    task complete

--- index.js
+++ index.js
@@ -15,52 +15,11 @@ let expressions = [
 
 let answer = [0, 350, -948, 92, -290, 5536, -13, 11, -8, 1920];
 
-const Calculator = {
-    calc: (expression) => {
-        let stack = [];
-        let tokens = expression.split(' ');
-
-        let counter = 0;
-        let counter2 = 0;
-        for(let i = 0; i < tokens.length; i++){
-            let token = tokens[i];
-
-            if(Calculator.isNumber(token)){
-                stack.push(parseInt(token));
-            }
-            if(Calculator.isOperand(token)){
-                stack.push(
-                    Calculator.doOperation(
-                        token,
-                        stack.pop(),
-                        stack.pop()
-                    )
-                );
-            }
-        }
-        return stack.pop();
-    },
-
-    doOperation: (operator, x, y) => {
-        return Calculator.operations[operator](y, x); 
-    },
-
-    operations: {
-        '+': (x, y) => x + y,
-        '-': (x, y) => x - y,
-        '*': (x, y) => x * y,
-        '/': (x, y) => x / y
-    },
-
-    isNumber: token => {
-        return /\d+/.test(token);
-    },
-
-    isOperand: token => {
-        return /[-\+\*\/]/.test(token);
+for (let i = 0; i < expressions.length; i++){
+    if(myPack(expressions[i]) == answer[i]){
+        console.log(expressions[i] + ' == ' + answer[i]);
     }
-}
-
-for(let i = 0; i < expressions.length; i++){
-    console.log(Calculator.calc(expressions[i]));
+    else{
+        console.log(expressions[i] + ' != ' + answer[i]);       
+    }
 }
